7ff296821aa1e94195ed8f6cd9d42d0182667ebf
Egwu15 <egwutedd@gmail.com>
2024-12-16 17:05:07 +0100

    fixed update

---
+++ public/build/assets/menu2-CKhbJCWz.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-DGAs3QsB.js";function r(){return e.jsx("div",{children:"menu2"})}export{r as default};
--- public/build/assets/signup-oa5CQGgz.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DRKigM8I.js";import t from"./Register-DuYhhu5V.js";import"./InputError-D-gXFP3P.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
