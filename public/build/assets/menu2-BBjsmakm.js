3577fc4e984e6f8e03cb1e5452c5e8ccdca0f0de
Egwu15 <egwutedd@gmail.com>
2024-12-16 16:17:44 +0100

    added build for cart

---
+++ public/build/assets/menu2-BBjsmakm.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-DK1uCf7Q.js";function r(){return e.jsx("div",{children:"menu2"})}export{r as default};
---
+++ public/build/assets/signup-Jh09fYWI.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DK1uCf7Q.js";import t from"./Register-BCVGZsdf.js";import"./InputError-CnPcdV3A.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
