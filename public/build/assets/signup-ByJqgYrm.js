6285890a2ed8305d0cabb568f4c302160bb7c771
Egwu15 <egwutedd@gmail.com>
2024-12-20 16:16:43 +0100

    add new build

---
+++ public/build/assets/signup-ByJqgYrm.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-BZT13Khd.js";import t from"./Register-AyeCl41t.js";import"./InputError-DF_rrDsV.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
--- public/build/assets/signup-GYh1hA3-.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DGAs3QsB.js";import t from"./Register-CQw8PEnj.js";import"./InputError-C6ThMAst.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
