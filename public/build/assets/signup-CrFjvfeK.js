2826d509233b20b737442c92b383eaa6b9e69475
Egwu15 <egwutedd@gmail.com>
2024-12-27 09:28:42 +0100

    build updated

--- public/build/assets/signup-CrFjvfeK.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BwN19FG1.js";import t from"./Register-C_9Y-0Jj.js";import"./InputError-CWGhwdhd.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
