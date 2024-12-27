ee959059e4ff4cddd50a72f9edd4672e17aee886
Egwu15 <egwutedd@gmail.com>
2024-12-27 20:42:05 +0100

    build update

--- public/build/assets/menu2-CkkH2z7H.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-CobX9PdW.js";function r(){return e.jsx("div",{children:"menu2"})}export{r as default};
--- public/build/assets/signup-dtAoZKny.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CobX9PdW.js";import t from"./Register-DVI689pd.js";import"./InputError-CGE4oUH1.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
