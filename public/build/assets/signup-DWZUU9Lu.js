6c14a23c73264cbcfa003770a610148fb46f5039
Egwu15 <egwutedd@gmail.com>
2025-01-07 15:30:27 +0100

    added build

---
+++ public/build/assets/signup-DWZUU9Lu.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DrSDzlc1.js";import t from"./Register-D2TCMn5l.js";import"./InputError-CeViplj3.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
