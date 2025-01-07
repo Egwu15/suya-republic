44d6c0949aef0503e2115efd0714ff3ffecb6682
Iyke <kennydyne@gmail.com>
2025-01-07 09:51:48 +0100

    fix

--- public/build/assets/signup-CJ0IwxqK.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CXrDWQMU.js";import t from"./Register-Dtb7_m3a.js";import"./InputError-D12zsEwA.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
