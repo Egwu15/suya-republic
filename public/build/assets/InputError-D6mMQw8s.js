35cc1ceb99dbd12621aafb50f5a3d56d0f63e568
Egwu15 <egwutedd@gmail.com>
2025-01-24 08:04:52 +0100

    build ready

--- public/build/assets/InputError-D6mMQw8s.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/InputError-CeViplj3.js
-import{j as e}from"./app-DrSDzlc1.js";function s({message:r,className:t="",...n}){return r?e.jsx("p",{...n,className:"text-sm text-red-600 "+t,children:r}):null}export{s as I};
-========
-import{j as e}from"./app-DIMLPhDE.js";function s({message:r,className:t="",...n}){return r?e.jsx("p",{...n,className:"text-sm text-red-600 "+t,children:r}):null}export{s as I};
->>>>>>>> main:public/build/assets/InputError-D6mMQw8s.js
--- public/build/assets/signup-B1zi9wY0.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CumzwSx0.js";import t from"./Register-BO_q8VnM.js";import"./InputError-oH8B2OF9.js";function e(){return r.jsx("div",{children:r.jsx(t,{})})}export{e as default};
