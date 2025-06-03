2be05dd02e80c07c8eaa5b48476db6afd8284c60
Egwu15 <egwutedd@gmail.com>
2025-06-03 17:29:10 +0100

    updated build

--- public/build/assets/Loader-DifObsAy.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-fryKYuo8.js";import{l as o}from"./Mobile-Logo-DjYHHJYk.js";const e=()=>s.jsx("div",{className:"loader-container",children:s.jsx("div",{className:"loader",children:s.jsxs("div",{className:"spinner",children:[" ",s.jsx("img",{src:o,alt:"Logo",className:"loader-logo"})]})})});export{e as L};
